5e4845d031f620356877a03a78c9dc0d709eb015
Liedel <liedel75@gmail.com>
2022-08-27 18:52:42 +0200

    add prettier tailwind css

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+                <Image
+                    src="https://rb.gy/vsvv2o"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
               <link rel="icon" href="/favicon.ico"/>
           </Head>
 
-          <h1>Lets build eshop</h1>
+          <Header/>
       </div>
   )
 }
