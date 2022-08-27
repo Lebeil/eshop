8021a37bcc117a2b48b2f3cc3602a5ccca22e159
Liedel <liedel75@gmail.com>
2022-08-27 17:54:45 +0200

    installation Sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,14 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
+      <div className="">
+          <Head>
+              <title>Eshop</title>
+              <link rel="icon" href="/favicon.ico"/>
+          </Head>
+
+          <h1>Lets build eshop</h1>
+      </div>
   )
 }
 
