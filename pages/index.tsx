865c3468280fbb4afda7ed650661ca360a8c360a
Liedel <liedel75@gmail.com>
2022-08-30 20:53:05 +0200

    add tab headlessui

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from "../components/Header";
+import Landing from "../components/Landing";
+import { Tab } from '@headlessui/react';
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +15,43 @@ const Home: NextPage = () => {
           </Head>
 
           <Header/>
+
+          <main className="relative  h-[200vh] bg-[#E7ECEE]">
+            <Landing/>
+          </main>
+          <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+              <div className="space-y-10 py-16">
+                  <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+                      New promos
+                  </h1>
+
+                  <Tab.Group>
+                      <Tab.List className="flex justify-center">
+                          {/*{categories.map((category) => (
+                              <Tab
+                                  key={category._id}
+                                  id={category._id}
+                                  className={({ selected }) =>
+                                      `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                                          selected
+                                              ? "borderGradient bg-[#35383C] text-white"
+                                              : "border-b-2 border-[#35383C] text-[#747474]"
+                                      }`
+                                  }
+                              >
+                                  {category.title}
+                              </Tab>
+                          ))}*/}
+                      </Tab.List>
+                      <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+                          <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+                          <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+                          <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+                          <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+                      </Tab.Panels>
+                  </Tab.Group>
+              </div>
+          </section>
       </div>
   )
 }
