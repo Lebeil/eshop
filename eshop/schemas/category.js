cc8319a9ea75bab9092a203a95bdfe30f327072b
Liedel <liedel75@gmail.com>
2022-09-02 16:21:57 +0200

    fix schema

--- eshop/schemas/category.js
+++ eshop/schemas/category.js
@@ -20,21 +20,5 @@ export default {
         maxLength: 96,
       },
     },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-    {
-      name: 'parents',
-      title: 'Parent categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'category'}],
-        },
-      ],
-    },
   ],
 }
