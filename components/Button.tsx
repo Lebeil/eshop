9befa3eff0ddec7879025ed16e7452a77ee7879b
Liedel <liedel75@gmail.com>
2022-09-22 22:22:07 +0200

    add react-hot-toast

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,7 @@ interface Props {
     noIcon?: boolean;
 }
 
-function Button({ title, onClick, width, loading, padding, noIcon }: Props) {
+function Button({title, onClick, width, loading, padding, noIcon}: Props) {
     return (
         <button
             className={`ease group relative z-30 box-border inline-flex ${
@@ -15,8 +15,10 @@ function Button({ title, onClick, width, loading, padding, noIcon }: Props) {
             } ${padding} cursor-pointer items-center justify-center overflow-hidden rounded bg-indigo-600 bg-gradient-to-r from-pink-500 to-violet-500 px-8 py-3 font-bold text-white transition-all duration-300 focus:outline-none`}
             onClick={onClick}
         >
-            <span className="absolute bottom-0 right-0 -mb-8 -mr-5 h-20 w-8 translate-x-1 rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
-            <span className="absolute top-0 left-0 -mt-1 -ml-12 h-8 w-20 -translate-x-1 -rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
+            <span
+                className="absolute bottom-0 right-0 -mb-8 -mr-5 h-20 w-8 translate-x-1 rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
+            <span
+                className="absolute top-0 left-0 -mt-1 -ml-12 h-8 w-20 -translate-x-1 -rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
 
             <span className="relative z-20 flex items-center font-semibold">
         {noIcon && (
@@ -32,11 +34,12 @@ function Button({ title, onClick, width, loading, padding, noIcon }: Props) {
                     strokeLinejoin="round"
                     strokeWidth="2"
                     d="M13 10V3L4 14h7v7l9-11h-7z"
-                ></path>
+                >
+                </path>
             </svg>
         )}
                 {loading ? "Loading..." : title}
-      </span>
+            </span>
         </button>
     );
 }
--- components/Product.tsx
+++ components/Product.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import Image from 'next/image'
 import {urlFor} from "../sanity";
 import {ShoppingCartIcon} from "@heroicons/react/outline";
+import {useDispatch} from "react-redux";
+import toast from "react-hot-toast";
+import {addToBasket} from "../redux/basketSlice";
 
 interface Props {
     product: Product
 }
 
 const Product = ({product}: Props) => {
+    const dispatch = useDispatch();
+
+    const addItemToBasket = ()=> {
+        dispatch(addToBasket(product));
+
+        toast.success(`${product.title} added to basket`, {
+            position: "bottom-center",
+        });
+    }
+
     return (
         <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
             <div className="relative h-64 w-full md:h-72">
@@ -25,7 +38,7 @@ const Product = ({product}: Props) => {
                 </div>
 
                 <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]"
-                    /*onClick={addItemToBasket}*/
+                    onClick={addItemToBasket}
                 >
                     <ShoppingCartIcon className="h-8 w-8 text-white"/>
                 </div>
